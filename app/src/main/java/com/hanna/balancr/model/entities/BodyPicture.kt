cb11e8f2c2975b477df41bb0df195934179aec0f
Hannakaroline <karolinepalacios@gmail.com>
2020-06-29 01:15:57 -0400

    Adiciona userId a modelo de foto de corpo

--- app/src/main/java/com/hanna/balancr/model/entities/BodyPicture.kt
+++ app/src/main/java/com/hanna/balancr/model/entities/BodyPicture.kt
@@ -4,10 +4,12 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.util.*
 
+
 @Entity(tableName = "body_pictures")
 data class BodyPicture(
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0,
     val date: Date,
-    val picturePath: String
+    val picturePath: String,
+    val userId: Long
 )
