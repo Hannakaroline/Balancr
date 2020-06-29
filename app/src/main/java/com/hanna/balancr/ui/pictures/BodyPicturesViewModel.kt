f9cc1f0254c3bf4ca1d774c7cdf12fa0e12ae418
Hannakaroline <karolinepalacios@gmail.com>
2020-06-29 00:57:30 -0400

    adicionado viewModel para fotos de corpo

--- app/src/main/java/com/hanna/balancr/ui/pictures/BodyPicturesViewModel.kt
+++ app/src/main/java/com/hanna/balancr/ui/pictures/BodyPicturesViewModel.kt
@@ -1,13 +1,23 @@
 package com.hanna.balancr.ui.pictures
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.*
+import com.hanna.balancr.model.BalancrDataBase
+import com.hanna.balancr.model.entities.BodyPicture
+import kotlinx.coroutines.launch
+import java.util.*
 
-class BodyPicturesViewModel : ViewModel() {
+class BodyPicturesViewModel(application: Application) : AndroidViewModel(application) {
+    private val database = BalancrDataBase.getInstance(application.applicationContext)
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+    val random = Random(1234)
+
+    val bodyPictures = database.bodyPictureDao().getBodyPictures()
+
+    fun addBodyPicture() {
+        viewModelScope.launch {
+            val bodyPicture = BodyPicture(date = Date(), picturePath = "x")
+            database.bodyPictureDao().insert(bodyPicture)
+        }
     }
-    val text: LiveData<String> = _text
 }
