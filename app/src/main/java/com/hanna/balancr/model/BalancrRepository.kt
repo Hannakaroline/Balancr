2552f9481358a51070ea7e3065e0b42816ecb8ad
Hannakaroline <karolinepalacios@gmail.com>
2020-06-29 00:59:42 -0400

    adiciona model do respositorio

---
+++ app/src/main/java/com/hanna/balancr/model/BalancrRepository.kt
@@ -0,0 +1,15 @@
+package com.hanna.balancr.model
+
+import com.hanna.balancr.model.daos.BodyPictureDao
+import com.hanna.balancr.model.daos.UserDao
+import com.hanna.balancr.model.daos.WeightingDao
+import com.hanna.balancr.model.entities.User
+import java.security.MessageDigest
+
+class BalancrRepository(
+    private val userDao: UserDao,
+    private val weightingDao: WeightingDao,
+    private val bodyPictureDao: BodyPictureDao
+) {
+
+}
