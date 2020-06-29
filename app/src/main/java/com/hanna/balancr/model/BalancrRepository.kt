621d1c32e6e09e8d8aa1871c8f0ea9b355fbdd04
Hannakaroline <karolinepalacios@gmail.com>
2020-06-29 01:15:34 -0400

    Refatora modelo de repositorio

--- app/src/main/java/com/hanna/balancr/model/BalancrRepository.kt
+++ app/src/main/java/com/hanna/balancr/model/BalancrRepository.kt
@@ -6,10 +6,10 @@ import com.hanna.balancr.model.daos.WeightingDao
 import com.hanna.balancr.model.entities.User
 import java.security.MessageDigest
 
-class BalancrRepository(
+class BalancrRepository (
     private val userDao: UserDao,
     private val weightingDao: WeightingDao,
     private val bodyPictureDao: BodyPictureDao
-) {
+){
 
 }
