5ef3435a84e5114bd5d873451d42dc2338e5f66e
Hannakaroline <karolinepalacios@gmail.com>
2020-06-29 01:17:42 -0400

    refatora viewModel de login para melhor autenticacao

--- app/src/main/java/com/hanna/balancr/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/hanna/balancr/ui/login/LoginViewModel.kt
@@ -3,9 +3,13 @@ package com.hanna.balancr.ui.login
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.viewModelScope
 import com.hanna.balancr.model.BalancrDataBase
+import com.hanna.balancr.model.entities.User
 import kotlinx.coroutines.launch
+import java.security.MessageDigest
+import java.util.*
 
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -15,36 +19,54 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
         InvalidAuthentication
     }
 
-    private val dataBase = BalancrDataBase.getInstance(application.applicationContext)
+    private val messageDigest = MessageDigest.getInstance("SHA-256")
+    private val database = BalancrDataBase.getInstance(application.applicationContext)
 
-    val authenticationState = MutableLiveData<AuthenticationState>()
+    val authenticationState = MutableLiveData(AuthenticationState.UnAuthenticated)
 
     init {
-        authenticationState.value = AuthenticationState.UnAuthenticated
+        viewModelScope.launch {
+            authenticationState.value = if (database.userDao().currentUser() == null) {
+                AuthenticationState.UnAuthenticated
+            } else {
+                AuthenticationState.Authenticated
+            }
+        }
     }
 
     fun authenticate(email: String, password: String) {
         viewModelScope.launch {
-            if (passwordIsValidForUsername(email, password)) {
+            val user = findUserFor(email, password)
+            if (user != null) {
+                val allUsers = database.userDao().getAllUsers()
+                database.userDao().updateUsers(allUsers.map { it.copy(isLoggedIn = false) })
+                database.userDao().updateUser(user.copy(isLoggedIn = true))
                 authenticationState.value = AuthenticationState.Authenticated
-
             } else {
                 authenticationState.value = AuthenticationState.InvalidAuthentication
             }
         }
     }
 
-    private suspend fun passwordIsValidForUsername(email: String, password: String): Boolean {
-        val user = dataBase.userDao().getUserByEmail(email)
-        return user != null
+    private suspend fun findUserFor(email: String, password: String): User? {
+        val user = database.userDao().getUserByEmail(email)
+        val digestedPassword = messageDigest.digest(password.toByteArray())
+        if (user != null && user.password == Arrays.toString(digestedPassword))
+            return user
+        return null
     }
 
     fun logout() {
-        this.authenticationState.value = AuthenticationState.UnAuthenticated
+        viewModelScope.launch {
+            val currentUser = database.userDao().currentUser()
+            currentUser?.let { user ->
+                database.userDao().updateUser(user.copy(isLoggedIn = false))
+            }
+            authenticationState.value = AuthenticationState.UnAuthenticated
+        }
     }
 
     fun refuseAuthentication() {
         this.authenticationState.value = AuthenticationState.UnAuthenticated
-
     }
 }
