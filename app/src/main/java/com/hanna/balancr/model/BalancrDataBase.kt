83ff33763230074f3d3687f4bf3b8668eed36a8f
Hannakaroline <karolinepalacios@gmail.com>
2020-06-29 01:15:00 -0400

    Refatora database

--- app/src/main/java/com/hanna/balancr/model/BalancrDataBase.kt
+++ app/src/main/java/com/hanna/balancr/model/BalancrDataBase.kt
@@ -1,12 +1,23 @@
 package com.hanna.balancr.model
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.hanna.balancr.model.daos.BodyPictureDao
 import com.hanna.balancr.model.daos.UserDao
 import com.hanna.balancr.model.daos.WeightingDao
+import com.hanna.balancr.model.entities.BodyPicture
+import com.hanna.balancr.model.entities.User
+import com.hanna.balancr.model.entities.Weighting
 
+@Database(
+    entities = arrayOf(User::class, Weighting::class, BodyPicture::class),
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(Converters::class)
 abstract class BalancrDataBase : RoomDatabase() {
     abstract fun userDao(): UserDao
     abstract fun weightingDao(): WeightingDao
