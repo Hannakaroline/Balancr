014af7b25413d07d84e30c941878001dd3a40c96
Hannakaroline <karolinepalacios@gmail.com>
2020-06-29 01:34:09 -0400

    Corrigi consulta sql de peso

--- app/src/main/java/com/hanna/balancr/model/daos/WeightingDao.kt
+++ app/src/main/java/com/hanna/balancr/model/daos/WeightingDao.kt
@@ -1,16 +1,18 @@
 package com.hanna.balancr.model.daos
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import com.hanna.balancr.model.entities.Weighting
 import java.sql.Date
 import java.util.*
 
+@Dao
 interface WeightingDao {
-    @Query("SELECT * FROM weightings WHERE date BETWEEN :start AND :end")
-    fun getWeightingsForPeriod(start: Date, end: Date): List<Weighting>
+    @Query("SELECT * FROM weightings WHERE userId = (SELECT id FROM users WHERE isLoggedIn = 1) ORDER BY date DESC, id DESC")
+    fun getCurrentUserWeightings(): LiveData<List<Weighting>>
 
     @Insert
-    fun insert(weighting: Weighting)
-
+    suspend fun insert(weighting: Weighting)
 }
