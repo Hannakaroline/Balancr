8fcdc340d9d49be28bff7068723bfc5afa216afd
Hannakaroline <karolinepalacios@gmail.com>
2020-06-29 01:35:26 -0400

    Corrigi consulta para foto de corpo

--- app/src/main/java/com/hanna/balancr/model/daos/BodyPictureDao.kt
+++ app/src/main/java/com/hanna/balancr/model/daos/BodyPictureDao.kt
@@ -1,17 +1,17 @@
 package com.hanna.balancr.model.daos
 
 import androidx.lifecycle.LiveData
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import com.hanna.balancr.model.entities.BodyPicture
+import com.hanna.balancr.model.entities.User
 import java.sql.Date
 
+@Dao
 interface BodyPictureDao {
-    @Query("SELECT * FROM body_pictures WHERE date BETWEEN :start AND :end")
-    fun getBodyPicturesForPeriod(start: Date, end: Date): List<BodyPicture>
-
-    @Query("SELECT * FROM body_pictures")
-    fun getBodyPictures(): LiveData<List<BodyPicture>>
+    @Query("SELECT * FROM body_pictures WHERE userId = (SELECT id FROM users WHERE isLoggedIn = 1)")
+    fun getCurrentUserBodyPictures(): LiveData<List<BodyPicture>>
 
     @Insert
     suspend fun insert(bodyPicture: BodyPicture)
